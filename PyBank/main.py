7f86a02051ba71ecd1040f050bd144dbc92fa546
Elyse Sulkey <elysesulkey@MacBook-Air.local>
2021-03-17 15:10:46 -0400

    pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,11 +6,19 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    month_count = -1
+    next(csvreader)
+
+    month_count = 0
+
+    profit_total = 0
 
     for row in csvreader:
-        month_count += 1
+        if row[0]:
+            month_count += 1
     
+        #if row[1]:
+           # profit_total =
+           
     print("Financial Analysis")
     print("______________________________________")
     print("Total Months: " + str(month_count))
