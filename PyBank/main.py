8e0caacf675ab641e00f1f842b8df23b567c1041
Elyse Sulkey <elysesulkey@MacBook-Air.local>
2021-03-17 17:49:28 -0400

    PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,24 +1,58 @@
 import os
 import csv
 
+#open csv and skip header info
+
 csvpath = os.path.join("Resources", "budget_data.csv")
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    next(csvreader)
+    csv_header = next(csvreader)
 
-    month_count = 0
+#list rows??
 
-    profit_total = 0
+#create calculation variables
+
+month_count = 0
+rev_total = 0
+rev_total_change = 0
+#greatest_increase = ???
+#greatest_decrease = ???
+previous_rev = 0
+month_change = []
+rev_change = []
+
+
+#loop through rows
+#get data from first row
+# go through each row
+#increase month count
+#add to profit/loss sum for total
+#calculate change in profit/loss (between months?) + find average
+#determine greatest increase
+#determine greatest decrease
 
     for row in csvreader:
         if row[0]:
             month_count += 1
     
-        #if row[1]:
-           # profit_total =
-           
+        if row[1]:
+            def accumulate (iterable, func=row[1].add, *, initial=None):
+                it = iter(iterable)
+                total = initial
+                if initial is None:
+                    try:
+                        total = next(it)
+                    except StopIteration:
+                        return
+                yield total
+                for element in it:
+                    total = func(total, element)
+                    yield total 
+        
+    #print analysis results
+            
     print("Financial Analysis")
     print("______________________________________")
     print("Total Months: " + str(month_count))
@@ -26,3 +60,6 @@ with open(csvpath) as csvfile:
     print("Average Change: ")
     print("Greatest Increase in Profits: ")
     print("Greatest Decrease in Profits: ")
+
+    #output to terminal
+    #output to file
