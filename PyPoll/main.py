39ca94427f1b9f2f7dea39c28aa8eff3d77e06c3
Elyse Sulkey <elysesulkey@MacBook-Air.local>
2021-03-19 17:25:53 -0400

    pypoll2

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-csvpath = os.path.join("Resources", "budget_data.csv")
+csvpath = os.path.join("Resources", "election_data.csv")
 
 #open csv and store/skip header info
 with open(csvpath) as csvfile:
