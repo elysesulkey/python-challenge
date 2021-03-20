a800ba343a5ddc07d5320dc5daf646bb70163832
Elyse Sulkey <elysesulkey@MacBook-Air.local>
2021-03-20 15:26:21 -0400

    3201

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,38 +10,38 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
 
     #create calculation variables
-
+    vote_count = 0
 
     #loop through rows
     for row in csvreader:
 
         #loop through voter ID & increase vote count
         if row[0]:
-            
-
+            vote_count += 1
+       
         #loop through candidates 
-        if row[2]:
+        #if row[2]:
 
         #list candidates (unique values?)
         #count votes each candidate received
         #percent vote each candidat received
             #candidate vote count / total vote count * 100
         #winner of election based on pop.vote (largest number)
-            if rev_change > increase_value:
+            #if rev_change > increase_value:
 
-                increase_month = row[0]
-                increase_value = rev_change
+                #increase_month = row[0]
+                #increase_value = rev_change
 
     #print analysis results
     analysis = (
         f"Election Results\n"
         f"______________________________________\n"
-        f"Total Votes: {month_count}\n"
+        f"Total Votes: {vote_count}\n"
         f"______________________________________\n"
-        f"Khan: {rev_total:,.2f}\n"
-        f"Correy: {avg_change:,.2f}\n"
-        f"Li: {increase_month}\n"
-        f"O'Tooley: {decrease_month}\n"
+        #f"Khan: {rev_total:,.2f}\n"
+        #f"Correy: {avg_change:,.2f}\n"
+        #f"Li: {increase_month}\n"
+        #f"O'Tooley: {decrease_month}\n"
         f"______________________________________\n"
         f"Winner: "
         f"______________________________________\n")
